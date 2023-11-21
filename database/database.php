e30e642630f252502146650a5a04fc0263166305
Christopher Lo <christopherlo383@gmail.com>
2023-11-21 13:54:25 -0800

    added .gitignore, started implementing .env variables

--- database/database.php
+++ database/database.php
@@ -1,9 +1,13 @@
 <?php
 
-$host = "poopmap-database.cdoxevnwyxjg.us-west-1.rds.amazonaws.com";
-$dbname = "poopmap-database";
-$username = "poopadmin";
-$password = "pooplovers18593";
+$env = parse_ini_file('.env');
+
+print($env['HOST']);
+
+$host = "poopmap-database.cdoxevnwyxjg.us-west-1.rds.amazonaws.com"; # $env["HOST"];
+$dbname = "poopmap"; #$env["DB_NAME"];
+$username = "poopadmin"; #$env["USER_NAME"];
+$password = "pooplovers18593"; #$env["PASSWORD"];
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
