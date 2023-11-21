ba9ad10ab2e0c0c8f176291c170081bdd5bb8819
Christopher Lo <christopherlo383@gmail.com>
2023-11-21 13:58:48 -0800

    added .env var usage

--- database/database.php
+++ database/database.php
@@ -2,12 +2,10 @@
 
 $env = parse_ini_file('.env');
 
-print($env['HOST']);
-
-$host = "poopmap-database.cdoxevnwyxjg.us-west-1.rds.amazonaws.com"; # $env["HOST"];
-$dbname = "poopmap"; #$env["DB_NAME"];
-$username = "poopadmin"; #$env["USER_NAME"];
-$password = "pooplovers18593"; #$env["PASSWORD"];
+$host = $env["HOST"];
+$dbname = $env["DB_NAME"];
+$username = $env["USER_NAME"];
+$password = $env["PASSWORD"];
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
