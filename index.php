1b7ca5674a6906146060269fbff84981ddf7b9d2
Justin Nguyen <98506939+chillwafflez@users.noreply.github.com>
2023-12-06 12:03:41 -0800

    Merging changes from Justin: added home page. poop markers display title, notes, datestamp, and timestamp. (#18)
    * added test login html file, will use .php instead
    
    * added html, css, and font files for login page. added login.php file but needs tweaking to work
    
    * changed name of database in database.php
    
    * added some changes. test.php was to test database connection
    
    * testing map
    
    * added index.php which uses google maps API to display poops onto the map using coordinates fetched from database for a specific user. corresponding js and css files added too
    
    * added using current user's ID to display their poops after logging in
    
    * random bracket?
    
    * fixed merge conflicts between justin and main
    
    * added functionality to create a new poop, which prompts for details and then adds it to database. poop_details.php is the prompt page
    
    * added date column to db. made infowindow for poop markers shows tag, note, and date
    
    * added home page. cleaned poop_details and index php files

--- index.php
+++ index.php
@@ -12,20 +12,9 @@ if (isset($_SESSION["currentID"])) {
     $result = $mysqli->query($sql);
     $poop_coord_array = [];
     while ($row = $result->fetch_assoc()) {
-        // echo $row["latitude"], " ", $row["longitude"], "<br>";
         array_push($poop_coord_array, [$row["latitude"], $row["longitude"], $row['tag'], $row['note'], $row['datePosted']]);
     }
 }
-
-// $mysqli = require __DIR__ . "/database/database.php";
-
-// $sql = sprintf("SELECT * FROM Pooper WHERE email = '%s'", $mysqli->real_escape_string($_POST["email"]));
-// $sql = sprintf("SELECT * FROM Pooper WHERE email = 'justin@gmail.com'");
-// $sql = sprintf("SELECT latitude, longitude, note FROM Poop where pooperid = 1;");
-
-// foreach ($poop_coord_array as $coordinate_pair) {
-//     echo $coordinate_pair[0], "  ", $coordinate_pair[1], '<br>';
-// }
 ?>
 
 <html>
@@ -33,15 +22,18 @@ if (isset($_SESSION["currentID"])) {
     <link rel = "stylesheet" href = "styles/index.css">
 </head>
 <body>
-    <div id="map"></div>
-    <a href="poop_details.php"><button id = "poop_button" >Drop poop</button></a>
+    <div id="outer_div">
+        <div id="map"></div>
+        <a href="poop_details.php"><button id="poop_button" class="button">Drop poop</button></a>
+        <a href="home.html"><button id="sign_out_button" class="button">Sign Out</button></a>
+    </div>
     <script type="text/javascript" src="scripts/index.js"></script>
-        <script type ='text/javascript'>
-            var passedArray =  <?php echo json_encode($poop_coord_array); ?>;  // convert PHP array into JS array
-            setCoords(passedArray);               // calls setCoords method in index.js (to set locations array) before initMap is called
-        </script>
-        <script async defer
-            src=<?php echo $apiCallString ?>>
-        </script>
+    <script type ='text/javascript'>
+        var passedArray =  <?php echo json_encode($poop_coord_array); ?>;  // convert PHP array into JS array
+        setCoords(passedArray);               // calls setCoords method in index.js (to set locations array) before initMap is called
+    </script>
+    <script async defer
+        src=<?php echo $apiCallString ?>>
+    </script>
 </body>
 </html>
