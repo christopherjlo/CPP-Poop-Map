90cf8ea6c8ed7b870fe58430814d7b7c76a2b21d
alonzojp <alonzojp@gmail.com>
2023-12-13 23:28:05 -0800

    Implemented viewing friend's poops

--- friends_poops.php
+++ friends_poops.php
@@ -7,32 +7,58 @@ session_start();
 
 if (isset($_SESSION["currentID"])) {
     $mysqli = require __DIR__ . "/database/database.php";
-    $sql = sprintf("SELECT latitude, longitude, tag, note, datePosted FROM Poop where pooperid = {$_SESSION["currentID"]};");
+
+    $stmt = sprintf("SELECT *  FROM Friends WHERE usr1id = " . $_SESSION["currentID"]);
+    $result = $mysqli->query($stmt);
+
+    $realFriends = []; // list of all your friends
+    while ($row = $result->fetch_assoc()) {
+
+        $stmt2 = sprintf("SELECT * FROM Friends WHERE usr1id = " . $row['usr2id'] . " AND usr2id = " . $_SESSION["currentID"]);
+        $result2 = $mysqli->query($stmt2);
+        $friend = $result2->fetch_assoc();
+
+        if (!empty($friend)) {
+            array_push($realFriends, $friend['usr1id']);
+        }
+    }
+
+    $sql = sprintf("SELECT Pooper.userid, Poop.latitude, Poop.longitude, Poop.tag, Poop.note, Poop.datePosted, Pooper.fName, Pooper.lName 
+    FROM Poop
+    INNER JOIN Pooper ON Poop.pooperid=Pooper.userid;");
 
     $result = $mysqli->query($sql);
     $poop_coord_array = [];
     while ($row = $result->fetch_assoc()) {
-        array_push($poop_coord_array, [$row["latitude"], $row["longitude"], $row['tag'], $row['note'], $row['datePosted']]);
+        if (in_array($row['userid'], $realFriends)) {
+            if ($row['userid'] == $_SESSION["currentID"]) {
+                array_push($poop_coord_array, [$row["latitude"], $row["longitude"], $row['tag'], $row['note'], $row['datePosted'], false, $row['fName'], $row['lName']]);
+            } else {
+                array_push($poop_coord_array, [$row["latitude"], $row["longitude"], $row['tag'], $row['note'], $row['datePosted'], true, $row['fName'], $row['lName']]);
+            }
+        }
     }
 }
 ?>
 
 <html>
+
 <head>
-    <link rel = "stylesheet" href = "styles/index.css">
+    <link rel="stylesheet" href="styles/index.css">
 </head>
+
 <body>
     <div id="outer_div">
         <div id="map"></div>
         <a href="home.html"><button id="sign_out_button" class="button">Sign Out</button></a>
     </div>
     <script type="text/javascript" src="scripts/friends_poops.js"></script>
-    <script type ='text/javascript'>
-        var passedArray =  <?php echo json_encode($poop_coord_array); ?>;  // convert PHP array into JS array
-        setCoords(passedArray);               // calls setCoords method in index.js (to set locations array) before initMap is called
+    <script type='text/javascript'>
+        var passedArray = <?php echo json_encode($poop_coord_array); ?>; // convert PHP array into JS array
+        setCoords(passedArray); // calls setCoords method in index.js (to set locations array) before initMap is called
     </script>
-    <script async defer
-        src=<?php echo $apiCallString ?>>
+    <script async defer src=<?php echo $apiCallString ?>>
     </script>
 </body>
+
 </html>
